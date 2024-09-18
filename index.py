06abeadf46a17dc3f7c35acec2b366d6279f19d5
Arthur <arthurschiller10@gmail.com>
2024-09-18 10:31:53 -0300

    teste

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
-# na hora que eu mandar o comando no wpp ele via rodar o programa e me dar informação que eu quero 
-
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
@@ -8,6 +6,17 @@ from bs4 import BeautifulSoup
 import pywhatkit
 import time
 from datetime import datetime, date
+from dotenv import load_dotenv
+import os
+
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Now you can access the variables
+email = os.getenv("EMAIL")
+senha = os.getenv("SENHA")
+db_password = os.getenv("DB_PASSWORD")
 
 
 
@@ -24,12 +33,8 @@ NAO = (By.ID, "idBtn_Back")
 BOTAOIBMEC = (By.XPATH, "//button[@aria-label='Entrar']")
 BOTAOMACRO = (By.XPATH, "//section[@id='card-entrega-IBM3293']//footer[@class='sc-ifdsxC iPMzmn']")
 BOTAOTRABALHO = (By.XPATH, "//button[@id='terceira-tab']")
-CARDTRABALHO = (By.XPATH, "//div[@class='sc-iRqkff bpukjd'][last()]//li//svg")
+CARDTRABALHO = (By.XPATH, "//button[@aria-label='Acessar Trabalho Lista de Exercícios - Cap 2']//i[@class='css-10ikh9f']//*[name()='svg']")
 DATA = (By.XPATH, "/html[1]/body[1]/div[1]/section[1]/section[1]/main[1]/section[1]/article[1]/section[1]/div[1]/div[1]/header[1]/div[1]/div[2]/div[1]/div[3]")
-
-
-# //button[@aria-label='Acessar Trabalho Lista de Exercícios - Cap 2']//i[@class='css-10ikh9f']//*[name()='svg'] -> o que dava certo para clicar no botao do card trabalho
-
 
 
 def main():
@@ -40,11 +45,11 @@ def main():
     #clicar no botão de logar no site do ibmec
     WebDriverWait(browser, 100).until(EC.element_to_be_clickable(BOTAOIBMEC)).click()
     # wait for email field and enter email
-    WebDriverWait(browser, 100).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys("seu email")
+    WebDriverWait(browser, 100).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys(email)
     # Click Next
     WebDriverWait(browser, 100).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
     # wait for password field and enter password
-    WebDriverWait(browser, 100).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys("sua senha")
+    WebDriverWait(browser, 100).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys(senha)
     # Click Login - same id?
     WebDriverWait(browser, 100).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
     #nao 
@@ -54,42 +59,10 @@ def main():
     #clicar no botao de trabalho
     WebDriverWait(browser, 100).until(EC.element_to_be_clickable(BOTAOTRABALHO)).click()
     #card trabalho
+    cardTabalho = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "sc-bbSZdi jMzIvF cardTrabalho")))
+    for o in cardTabalho:
+        cardTabalho.click()
+        print(o)
+    
 
-    
-       
-    WebDriverWait(browser, 100).until(EC.element_to_be_clickable(CARDTRABALHO)).click()
-    elements = WebDriverWait(browser, 100).until(EC.presence_of_all_elements_located((By.XPATH, "//p[contains(text(),'Não é possível enviar um trabalho. O prazo se ence')]")))
-    x = elements[0].text
-    wordData = x.split()
-    dataReal = wordData[-1]
-    dataReal = dataReal[:-1]
-    format = "%d/%m/%Y"
-    dataa = datetime.strptime(dataReal, format)
-    if datetime.date(dataa) == datetime.today().date():
-        print(f'Hoje, {dataReal}, tem atividade para entregar')
-    else:
-        print('nao tem atividade para hoje')
-        # WebDriverWait(browser, 100).until(EC.element_to_be_clickable((By.CLASS_NAME, "sc-cPiKLX czwAXI"))).click()
-    
-       #WebDriverWait(browser, 100).until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Acessar página inicial']//i[@class='css-10ikh9f']//*[name()='svg']"))).click()
-
-    
 main()
-
-
-
-#css selector e for function
-    
-
-    
-#print(dataReal)
-    
-# if __name__ == '__main__':
-#     while True:
-#         main()
-#         time_wait = 1440
-#         print(f'Waiting {time_wait} minuts...')
-#         time.sleep(time_wait * 60)
-
-
-
